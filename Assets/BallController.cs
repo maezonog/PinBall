74a3361c5263ef8f303d47f3a4fe968560c6cd8b
maezono <maezonog@gmail.com>
2019-01-25 00:50:30 +0900

    pinball課題と発展課題の作成
    発展課題のタッチパネルのコードがあまり綺麗にはできませんでした。（作動はする）

---
+++ Assets/BallController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	private float visiblePosZ = -6.5f;
+
+	private GameObject gameoverText;
+	private GameObject scoreText;
+	private int sum = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		this.gameoverText = GameObject.Find ("GameOverText");
+		this.scoreText = GameObject.Find ("ScoreText");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (this.transform.position.z < this.visiblePosZ) {
+			this.gameoverText.GetComponent<Text> ().text= "Game Over";
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "SmallStarTag"){
+			sum += 10;
+			this.scoreText.GetComponent<Text> ().text = "Score " + sum;
+		}
+		if (other.gameObject.tag == "LargeStarTag"){
+			sum += 20;
+			this.scoreText.GetComponent<Text> ().text = "Score " + sum;
+		}
+		if (other.gameObject.tag == "SmallCloudTag"){
+			sum += 50;
+			this.scoreText.GetComponent<Text> ().text = "Score " + sum;
+		}
+		if (other.gameObject.tag == "LargeCloudTag"){
+			sum += 100;
+			this.scoreText.GetComponent<Text> ().text = "Score " + sum;
+		}
+	}
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+	private HingeJoint myHingeJoint;
+	private float defaultAngle = 20;
+	private float flickAngle= -20;
+
+
+	// Use this for initialization
+	void Start () {
+
+		this.myHingeJoint = GetComponent<HingeJoint> ();
+		SetAngle (this.defaultAngle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			for (int i = 0; i < Input.touchCount; i++) {
+				var id = Input.touches [i].fingerId;
+				var pos = Input.touches [i].position;
+				Debug.LogFormat ("{0} - x:{1}, y:{2}", id, pos.x, pos.y);
+				if (pos.x < 350 && tag == "LeftFripperTag")
+					SetAngle (this.flickAngle);
+				if (pos.x >= 350 && tag == "RightFripperTag")
+					SetAngle (this.flickAngle);
+			}
+
+		} else 
+			SetAngle (this.defaultAngle);
+			
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+
+	}
+
+		public void SetAngle(float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+
+		
+	}
+}
